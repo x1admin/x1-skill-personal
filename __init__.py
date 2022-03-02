1341bd2dbec9445a30579dda99b4f4e6ed5a40dd
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-03-02 02:03:42 +0000

    workflows (#3)
    authored-by: jarbasai <jarbasai@mailfence.com>

--- __init__.py
+++ __init__.py
@@ -1,21 +1,8 @@
-# Copyright 2017 Mycroft AI Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from mycroft.skills import MycroftSkill, intent_handler
+from ovos_workshop.skills import OVOSSkill
+from mycroft.skills import intent_handler
 
 
-class PersonalSkill(MycroftSkill):
+class PersonalSkill(OVOSSkill):
     @intent_handler("WhenWereYouBorn.intent")
     def handle_when_were_you_born_intent(self, message):
         self.speak_dialog("when.was.i.born")
--- setup.py
+++ setup.py
@@ -1,8 +1,50 @@
 #!/usr/bin/env python3
 from setuptools import setup
+import os
+
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
 
 # skill_id=package_name:SkillClass
-PLUGIN_ENTRY_POINT = 'mycroft-personal.mycroftai=ovos_skill_personal:PersonalSkill'
+PLUGIN_ENTRY_POINT = 'ovos-skill-personal.OpenVoiceOS=ovos_skill_personal:PersonalSkill'
 # in this case the skill_id is defined to purposefully replace the mycroft version of the skill,
 # or rather to be replaced by it in case it is present. all skill directories take precedence over plugin skills
 
@@ -10,16 +52,17 @@ PLUGIN_ENTRY_POINT = 'mycroft-personal.mycroftai=ovos_skill_personal:PersonalSki
 setup(
     # this is the package name that goes on pip
     name='ovos-skill-personal',
-    version='0.0.1',
+    version=get_version(),
     description='OVOS personal skill plugin',
-    url='https://github.com/OpenVoiceOS/skill-personal',
+    url='https://github.com/OpenVoiceOS/ovos-skill-personal',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
     package_dir={"ovos_skill_personal": ""},
-    package_data={'ovos_skill_personal': ["dialog/*", "vocab/*"]},
+    package_data={'ovos_skill_personal': ["locale/*"]},
     packages=['ovos_skill_personal'],
     include_package_data=True,
+    install_requires=required("requirements.txt"),
     keywords='ovos skill plugin',
     entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
 )
